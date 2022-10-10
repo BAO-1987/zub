566c9e1f753ffc834edeafb0e27f448cf1e9776a
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2022-10-10 22:55:05 +0300

    aaa

--- src/js/components/_tabs.js
+++ src/js/components/_tabs.js
@@ -1,5 +1,9 @@
 import GraphTabs from 'graph-tabs';
-const tabs = new GraphTabs('gallery');
+// const tabs = new GraphTabs('gallery');
+
+if (document.querySelector('.gallery')) {
+  const tabs = new GraphTabs('.gallery')
+}
 
 
 
