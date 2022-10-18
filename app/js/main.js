bda99361d6a56eb57c5c2bd48617a880e2cf180d
BAO <andriy.bezkorovajnyj@gmail.com>
2022-10-18 16:52:15 +0300

    animation

--- app/js/main.js
+++ app/js/main.js
@@ -175,21 +175,100 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var scrollreveal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! scrollreveal */ "./node_modules/scrollreveal/dist/scrollreveal.es.js");
+ // ScrollReveal().reveal('.header', {
+//   duration: 1000,
+//   distance: "10%",
+//   easing: "ease",
+//   delay: "800",
+//   origin: "top",
+//   // reset: true,
+// });
 
-(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.title', {
-  reset: true,
-  delay: 500,
-  origin: 'bottom',
-  distance: '20px',
-  easing: "ease-out"
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal(".hero__title", {
+  duration: 1000,
+  distance: "20px",
+  easing: "ease",
+  delay: "200",
+  origin: "top",
+  reset: true
 });
-(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.stages__list, .implantation__box, .content, .text, .advantages__list, .services__list, .cad-cam__box, .dentures__list, .services__list, .gallery, .reviews-slider, .news__list, .address, .contacts-form, .profit__content, .maps', {
-  reset: true,
-  delay: 600,
-  origin: 'bottom',
-  distance: '40px',
-  easing: "ease-out"
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal(".text", {
+  duration: 1200,
+  distance: "20px",
+  easing: "ease",
+  delay: "100",
+  origin: "top",
+  reset: true
 });
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.content, .gallery__list, .reviews-slider,.news-slider, .cad-cam__box', {
+  duration: 800,
+  distance: "10%",
+  easing: "ease",
+  delay: "500",
+  origin: "top",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.services__more, .news__more, .reviews__more, gallery__more, .slider-btn, .btn--second', {
+  duration: 700,
+  distance: "10%",
+  easing: "ease",
+  delay: "500",
+  origin: "top",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.contacts__inner', {
+  duration: 700,
+  distance: "10%",
+  easing: "ease",
+  delay: "500",
+  origin: "bottom",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal('.footer', {
+  duration: 800,
+  distance: "10%",
+  easing: "ease",
+  delay: "500",
+  origin: "bottom",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal(".title", {
+  duration: 500,
+  distance: "20%",
+  easing: "ease",
+  delay: "100",
+  origin: "top",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal(".dentures__title", {
+  duration: 600,
+  distance: "20%",
+  easing: "ease",
+  delay: "150",
+  origin: "top",
+  reset: true
+});
+(0,scrollreveal__WEBPACK_IMPORTED_MODULE_0__["default"])().reveal(".advantages__list, .dentures__list, .services__list, .stages__list", {
+  duration: 600,
+  distance: "20px",
+  easing: "ease",
+  delay: "100",
+  origin: "top",
+  reset: true
+}); // ScrollReveal().reveal('.title', {
+//   reset: true,
+//   duration: 900,
+//   origin: 'bottom',
+//   distance: '20px',
+//   easing: "ease-out",
+// });
+// ScrollReveal().reveal('.stages__list, .implantation__box, .content+, .text+, .advantages__list, .services__list, .cad-cam__box, .dentures__list, .services__list, .gallery, .reviews-slider, .news__list, .address, .contacts-form, .profit__content, .maps', {
+//   reset: true,
+//   duration: 700,
+//   origin: 'bottom',
+//   distance: '40px',
+//   easing: "ease-out",
+// });
 
 /***/ }),
 
