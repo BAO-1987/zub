fe697d64ba2879b1eb54e3abc13a6c72e32b8c4d
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2022-10-30 12:41:23 +0200

    add news page

--- src/js/components/_animation.js
+++ src/js/components/_animation.js
@@ -1,12 +1,12 @@
 import ScrollReveal from "scrollreveal";
 ScrollReveal({
   reset: true,
+  easing: "ease",
 });
 
 ScrollReveal().reveal(".hero__title", {
   duration: 1000,
   distance: "20px",
-  easing: "ease",
   delay: "200",
   origin: "top",
 });
@@ -14,7 +14,6 @@ ScrollReveal().reveal(".hero__title", {
 ScrollReveal().reveal(".text", {
   duration: 1200,
   distance: "20px",
-  easing: "ease",
   delay: "100",
   origin: "top",
 });
@@ -23,16 +22,14 @@ ScrollReveal().reveal(
   ".content, .reviews-slider, .news__item, .news-slider, .cad-cam__box, .implantation__box", {
     duration: 700,
     distance: "10px",
-    easing: "ease",
-    delay: "700",
+    delay: "200",
     origin: "top",
   }
 );
 
 ScrollReveal().reveal(".gallery__list> li", {
   duration: 500,
-  distance: "20%",
-  easing: "ease",
+  distance: "10%",
   delay: "100",
   origin: "top",
   interval: 200,
@@ -43,7 +40,6 @@ ScrollReveal().reveal(
   ".services__more, .news__more, .reviews__more, .gallery__more, .slider-btn, .btn--second", {
     duration: 500,
     distance: "10px",
-    easing: "ease",
     delay: "100",
     origin: "top",
   }
@@ -52,7 +48,6 @@ ScrollReveal().reveal(
 ScrollReveal().reveal(".btn--show-more", {
   duration: 800,
   distance: "10px",
-  easing: "ease",
   delay: "100",
   origin: "top",
   opacity: 1,
@@ -62,7 +57,6 @@ ScrollReveal().reveal(".btn--show-more", {
 ScrollReveal().reveal(".contacts__inner, .pagination", {
   duration: 700,
   distance: "10px",
-  easing: "ease",
   delay: "500",
   origin: "top",
 });
@@ -80,11 +74,11 @@ ScrollReveal().reveal(".tabs__nav> li> *", {
 
 ScrollReveal().reveal(".gallery-main__list> li", {
   duration: 600,
-  distance: "15px",
+  distance: "20%",
   easing: "ease",
-  delay: "100",
-  origin: "top",
-  interval: 100,
+  delay: "200",
+  origin: "bottom",
+  // interval: 50,
   opacity: 1,
   mobile: false,
 });
